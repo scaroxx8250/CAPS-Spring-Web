38c2acd48ff945d36352ae5678bbfee3417dd385
learner-9 <e0699394@u.nus.edu>
2021-06-20 12:13:56 +0800

    First check

---
+++ CAPSProj/src/test/java/com/team6/CAPSProj/CapsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.CAPSProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
