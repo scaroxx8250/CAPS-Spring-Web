183b99285e9ebc116c354dc4b525d2c07744c929
stepko-dev <e0696877@u.nus.edu>
2021-06-21 16:28:56 +0800

    test commit stephan

--- CAPSProj/src/main/java/com/team6/CAPSProj/CapsProjApplication.java
+++ CAPSProj/src/main/java/com/team6/CAPSProj/CapsProjApplication.java
@@ -9,6 +9,7 @@ public class CapsProjApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CapsProjApplication.class, args);
 		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 
 }
