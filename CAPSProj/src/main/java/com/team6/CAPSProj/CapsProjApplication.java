c4fd292ae2930a93ad894f560ba17e62e69a646f
Edmund-Koo <e0696911@u.nus.edu>
2021-06-21 16:59:16 +0800

    8th try

--- CAPSProj/src/main/java/com/team6/CAPSProj/CapsProjApplication.java
+++ CAPSProj/src/main/java/com/team6/CAPSProj/CapsProjApplication.java
@@ -8,7 +8,7 @@ public class CapsProjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapsProjApplication.class, args);
-		System.out.println("Hello world7");
+		System.out.println("Hello world8");
 	}
 
 }
