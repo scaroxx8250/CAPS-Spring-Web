66bbae9754aef1686ec7292cb6977567d64e3212
stepko-dev <e0696877@u.nus.edu>
2021-06-23 11:57:54 +0800

    StudentService Implementation

--- CAPSProj/src/main/java/com/team6/CAPSProj/repo/StudentRepository.java
+++ CAPSProj/src/main/java/com/team6/CAPSProj/repo/StudentRepository.java
@@ -1,9 +1,17 @@
 package com.team6.CAPSProj.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team6.CAPSProj.model.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+	
+	public Student findByMatricNo(String matricNo);
+
+	public Student findByEmail(String email);
+
+	public List<Student> findByFirstName(String input);
 
 }
